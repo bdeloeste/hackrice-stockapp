ce1f006804b290a3b5e1742f1caab984a2e7de7c
Bryan Deloeste <bryandeloeste@gmail.com>
2016-01-16 12:18:36 -0600

    update proj

--- stockapp/stocks/views.py
+++ stockapp/stocks/views.py
@@ -1,3 +1,14 @@
+import tweepy
+
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    google_counter = 0
+    twitter_counter = 0
+    facebook_counter = 0
+
+    co_stocks = {}
+
+    return render(request, 'base.html', {})
