09b7d296c61183568fab06a644c2b96501dd59ff
Justin Hill <jhillkwaj@gmail.com>
2016-01-16 13:51:41 -0600

    Added code for sentement analysis

---
+++ nltk/nltk_test.py
@@ -0,0 +1,27 @@
+import nltk;
+from nltk.classify import  NaiveBayesClassifier
+from nltk.sentiment import  SentimentAnalyzer
+from nltk.sentiment.util import *
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import subjectivity
+
+from nltk import tokenize
+
+
+def getSentiments(sentences):
+    from nltk import tokenize
+    sid = SentimentIntensityAnalyzer()
+    sentimtents = [None] * len(sentences)
+    i = 0
+    for sentence in sentences:
+        ss = sid.polarity_scores(sentence)
+        sentimtents[i] = ss['compound']
+        i = i + 1
+    return sentimtents
+
+
+
+sen = ["Don't wanna RT a bunch of @realDonaldTrump... but Donald is carpet bombing @tedcruz right now and it's really entertaining.", "Greatly dishonest of @TedCruz to file a financial disclosure form & not list his lending banks- then pretend he is going to clean up Wall St"]
+print(getSentiments(sen))
+
+
